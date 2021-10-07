058e2f84ad1d17f9804af19a0b616df0b753830e
lumingrui <lomirus@qq.com>
2021-10-08 00:59:44 +0800

    feat: basic functions

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,78 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+    let input = "";
+    let output = "";
+
+    async function update() {
+        try {
+            const res = await fetch("http://wuhaoda.life:11451/info", {
+                method: "POST",
+                body: JSON.stringify({
+                    data: input,
+                }),
+            });
+            const json: Wrapper = await res.json();
+            output = json.data;
+        } catch (err) {
+            alert(err)
+        }
+    }
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+    <div class="wrapper">
+        <textarea bind:value={input} placeholder="Input Something here..." />
+        <textarea bind:value={output} placeholder="Here is the output" />
+    </div>
+    <button on:click={update}>✈️</button>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
+    :root {
+        margin: 40px;
+        overflow: hidden;
     }
 
-    p {
-      max-width: none;
+    .wrapper {
+        display: grid;
+        height: calc(100vh - 80px);
+        column-gap: 80px;
+        grid-template-columns: 1fr 1fr;
+        width: 100%;
     }
-  }
+
+    textarea {
+        padding: 12px;
+        outline: none;
+        border-radius: 5px;
+        border-color: #dcdfe6;
+        resize: none;
+    }
+
+    textarea:focus {
+        transition: all ease 0.1s;
+        border-color: #409eff;
+    }
+
+    button {
+        position: fixed;
+        right: 50px;
+        bottom: 50px;
+        transform: translate(-50%, -50%);
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+        border: none;
+        background: #409eff;
+        color: #ffffff;
+        font-size: 32px;
+        cursor: pointer;
+        transition: all ease 0.1s;
+    }
+
+    button:hover {
+        background-color: #66b1ff;
+    }
+    button:active {
+        background-color: #0d84ff;
+    }
 </style>
